9b70f407390121515e87272eabc1a74bff2c9025
rulala <rulala212@gmail.com>
2025-10-29 16:28:55 +0900

    v0 q

--- 09.py
+++ 09.py
@@ -3,10 +3,16 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-titanic = sns.load_dataset('titanic')
+#titanic = sns.load_dataset('titanic')
 # print(titanic.info())
 # print(titanic.tail(3))
-print(titanic.query('age >= 70'))
+#print(titanic.query('age >= 70'))
 
-sns.scatterplot(x='age',y='fare',data=titanic)
-plt.show()
+#sns.scatterplot(x='age',y='fare',data=titanic)
+#plt.show()
+df=sns.load_dataset('penguins')
+#print(df.info())
+#print(df.head())
+#print(df.describe())
+#print(df['body_mass_g'].describe())
+print(df['bill_length_mm']>= 55.0)
